7ab5a2da21e774c14c225dd708f86ae29b84d0de
mithilesh2023 <mithilesht2023@gmail.com>
2023-10-04 22:30:34 +0530

    login register completed

---
+++ src/ClassComp.js
@@ -0,0 +1,9 @@
+import React,{Component} from "react";
+class component1 extends Component{
+  render(){
+     return(
+        <h1> This is our first project.</h1>
+         )
+	   }
+    }
+export default  component1;
