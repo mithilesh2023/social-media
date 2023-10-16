2de3d85d6dd2477d0f8a41c38d45819b36ac094a
mithilesh2023 <mithilesht2023@gmail.com>
2023-10-16 19:39:03 +0530

    all deleted

--- src/App.js
+++ src/App.js
@@ -1,77 +1,11 @@
-import React, { useContext } from 'react';
-import Login from './pages/login/Login';
-import Register from './pages/register/Register';
-import { Navigate, Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom';
-import Navbar from './components/navbar/Navbar';
-import LeftBar from './components/leftBar/LeftBar';
-import RightBar from './components/rightBar/RightBar';
-import Home from './pages/home/Home';
-import Profile from './pages/profile/Profile';
-import { DarkModeContext } from './context/darkModeContext';
-import { AuthContext } from './context/authContext';
-function App() {
-
-const currentUser=useContext(AuthContext);
-
-const {darkMode}=useContext(DarkModeContext);
-
-console.log(darkMode)
-
-  const Layout=()=>{
+import React from 'react';
+import './App.css';
+  const App=()=>{
     return(
 
-      <div className={`theme-${darkMode ? "dark":"light"}`}>
-        <Navbar/>
-        <div style={{display:"flex"}}>
-          <LeftBar/>
-          <div style={{flex:6}}>
-          <Outlet/>
-          </div>
-          <RightBar/>
-        </div>
+      <div className='App'>
+       
       </div>
     )
   }
-
-  const ProtectedRoute=({children})=>{
-    if(!currentUser){
-      return <Navigate to="/login"/>
-    }
-    return children
-  }
-
-  const router=createBrowserRouter([
-    {
-      path:"/",
-      element:
-      <ProtectedRoute> 
-         <Layout/>
-      </ProtectedRoute>,
-      children:[
-        {
-          path:"/",
-          element:<Home/>,
-      },
-      {
-        path:"/profile/:id",
-        element:<Profile/>
-      }
-      ]
-    },
-    {
-      path:"/login",
-      element:<Login/>,
-    },
-    {
-      path:"/register",
-      element:<Register/>,
-    }
-  ]);
-  return (
-   <>
-  <RouterProvider router={router}/>
-   </>
-  );
-}
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,16 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { DarkModeContextProvider } from './context/darkModeContext';
-import { AuthContextProvider } from './context/authContext';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <DarkModeContextProvider>
-    <AuthContextProvider>
       <App />
-    </AuthContextProvider>
-  </DarkModeContextProvider>
- 
 );
 
